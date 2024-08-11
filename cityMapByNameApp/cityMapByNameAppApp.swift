2842aecc841a04d57e42f383e100aa857a336ee6
csmohmmsdsh <mohmmad521@hotmail.com>
2024-08-12 00:41:56 +0300

    Initial Commit

---
+++ cityMapByNameApp/cityMapByNameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  cityMapByNameAppApp.swift
+//  cityMapByNameApp
+//
+//  Created by CSmohmmadSH on 12/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct cityMapByNameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
